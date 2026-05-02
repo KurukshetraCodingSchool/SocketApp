00727058112c86c6777a8d14a46ed0bd8d58b5f2
kurukshetra <kurukshetracodingschool.com>
2026-05-02 22:19:51 +0530

    joinrromm

--- config/db.js
+++ config/db.js
@@ -1,14 +1,18 @@
 const mongoose=require('mongoose')
-const mongodb= mongoose.connect('mongodb://0.0.0.0/Socketapp').then(()=>{
-    console.log('db connected✅');
+const mongodb= mongoose.connect('mongodb://0.0.0.0/joinroom').then(()=>{
+    console.log('connected✅');
 }).catch((error)=>{
     console.log(error);
 })
 const socketSchema =  mongoose.Schema({
-    content: String,
-    timestamp:{
-        type:Date,
-        default:Date.now
-    }
+    username : String,
+    msg : String,
+    roomName:String,
+    // timestamps:{
+    //     type:Date,
+    //     default:Date.now
+    // }
+},{
+    timestamps:true
 })
 module.exports =mongoose.model('Chat',socketSchema);
--- socketio.js
+++ socketio.js
@@ -2,22 +2,36 @@ const io = require('socket.io')();
 const Message = require('./config/db');
 
 const socketapi = {
-    io:io
+    io: io
 }
 // Logic Setup
-io.on('connection',async function(socket){
+io.on('connection', async function (socket) {
     console.log("new User Connected");
-    const messages = await Message.find().sort({timestamp:1}).limit(50);
+    // const messages = await Message.find().sort({timestamp:1}).limit(50);
 
-    messages.forEach(msg=>{
-        socket.emit('incomingMessage',msg.content)
+    socket.on('joinRoom', async (roomName) => {
+        socket.join(roomName);
+
+        // Sends The 100 msg
+            const messages = await Message.find({roomName}).sort({timestamp:1}).limit(100)
+
+            messages.forEach(msg => {
+                socket.emit('incomingMessage', msg) // Past user data Send And Show 
+    })
     })
 
-    socket.on('newmsg', async msg=>{
-        socket.broadcast.emit('incomingMessage',msg)
+    socket.on('deleteMessage',async (msgId)=>{
+        await Message.findByIdAndDelete(msgId);
+        console.log('messagedelete',msgId);
+        // all useer see thw delete 
+        socket.broadcast.emit('messageDeleted',msgId)
+    });
 
-        const message = new Message({content:msg});
-        await message.save();
+    socket.on('sony', async (msg) => {
+        const newmsg = new Message(msg);
+        const savedmsg = await newmsg.save();
+        // socket.broadcast.to(msg.roomName).emit('incomingMessage',msg);
+        io.to(msg.roomName).emit('incomingMessage',savedmsg);
     })
 })
 module.exports = socketapi
